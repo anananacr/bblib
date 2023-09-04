53597283099d6d238aa18166252b78cf5f05f393
Ana Rodrigues <anananacr@gmail.com>
2023-09-04 12:48:19 +0200

    feat: bacth script for centering f2x

--- scripts/algorithms/batch_f2x_center.sh
+++ scripts/algorithms/batch_f2x_center.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 #SBATCH --partition=allcpu,cfel
-#SBATCH --time=0-23:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --requeue
 #SBATCH --nodes=1
-#SBATCH --nice=128
-#SBATCH --mincpus=100
+#SBATCH --nice=0
+#SBATCH --mincpus=48
 #SBATCH --mem=4G
 
 #SBATCH --job-name  f2x_center
 #SBATCH --output    /gpfs/cfel/user/rodria/processed/p09/11016750/error/center-%N-%j.out
 #SBATCH --error     /gpfs/cfel/user/rodria/processed/p09/11016750/error/center-%N-%j.err
 
+ROOT=/asap3/petra3/gpfs/p09/2023/data/11016750/processed/rodria
 source /etc/profile.d/modules.sh
 module load maxwell python/3.7
 source /home/rodria/scripts/p09/env-p09/bin/activate
-cp /gpfs/cfel/user/rodria/processed/p09/11016750/geom/pilatus6M_219mm.geom /asap3/petra3/gpfs/p09/2023/data/11016750/processed/rodria/geom/f2x_04_03
-./find_center.py -i /gpfs/cfel/user/rodria/processed/p09/11016750/lists/f2x_chip_04_fly_03.lst -m  /gpfs/cfel/user/rodria/processed/p09/11016750/lists/mask_f2x.lst -o /asap3/petra3/gpfs/p09/2023/data/11016750/processed/rodria/f2x_04_03/f2x_04_03 -g /asap3/petra3/gpfs/p09/2023/data/11016750/processed/rodria/geom/f2x_04_03/pilatus6M_219mm.geom;
+cp /gpfs/cfel/user/rodria/processed/p09/11016750/geom/pilatus6M_219mm.geom ${ROOT}/geom/f2x_D_01_01
+./find_center.py -i ${ROOT}/f2x_D_01_01/lists/split_f2x_D_01_01.lst10 -m  ${ROOT}/f2x_D_01_01/lists/mask_f2x.lst -o ${ROOT}/f2x_D_01_01 -g ${ROOT}/geom/f2x_D_01_01/pilatus6M_219mm.geom;

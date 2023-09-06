5f47a84e0542b5cf9bfd40b40c05f7e2a0d75640
Ana Rodrigues <anananacr@gmail.com>
2023-09-06 17:26:10 +0200

    feat: zip N random images of the cluster folder

--- scripts/algorithms/visual_check_cluster.sh
+++ scripts/algorithms/visual_check_cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-PATH=/asap3/petra3/gpfs/p09/2023/data/11016750/processed/rodria/f2x_D_01_01/cluster_0
+PATH=/asap3/petra3/gpfs/p09/2023/data/11016750/processed/rodria/f2x_D_01_01/cluster_2
 N=500
 
 /usr/bin/ls $PATH| /usr/bin/sort -R| /usr/bin/tail -$N | while read file; do
-/usr/bin/zip $PATH/../cluster_0.zip $PATH/$file 
+/usr/bin/zip $PATH/../cluster_2.zip $PATH/$file 
 done

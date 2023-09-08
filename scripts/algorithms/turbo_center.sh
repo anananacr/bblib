f7581d8c5a025e329494a9bd3f07cccc918a8035
Ana Rodrigues <anananacr@gmail.com>
2023-09-08 10:47:44 +0200

    fix: speed up

--- scripts/algorithms/turbo_center.sh
+++ scripts/algorithms/turbo_center.sh
@@ -13,7 +13,7 @@ for i in $(seq $START 1 $END); do
     echo "#!/bin/sh" > $SLURMFILE
     echo >> $SLURMFILE
     echo "#SBATCH --partition=allcpu,cfel" >> $SLURMFILE  # Set your partition here
-    echo "#SBATCH --time=1-00:00:00" >> $SLURMFILE
+    echo "#SBATCH --time=2-00:00:00" >> $SLURMFILE
     echo "#SBATCH --nodes=1" >> $SLURMFILE
     echo >> $SLURMFILE
     echo "#SBATCH --chdir   $PWD" >> $SLURMFILE
@@ -22,7 +22,7 @@ for i in $(seq $START 1 $END); do
     echo "#SBATCH --output    $ROOT/error/${NAME}-%N-%j.out" >> $SLURMFILE
     echo "#SBATCH --error     $ROOT/error/${NAME}-%N-%j.err" >> $SLURMFILE
     echo "#SBATCH --nice=0" >> $SLURMFILE
-    echo "#SBATCH --mincpus=48" >> $SLURMFILE
+    echo "#SBATCH --mincpus=128" >> $SLURMFILE
     echo "#SBATCH --mem=4G" >> $SLURMFILE
     echo "#SBATCH --mail-type=ALL" >> $SLURMFILE
     echo "#SBATCH --mail-user=errodriguesana@gmail.com" >> $SLURMFILE

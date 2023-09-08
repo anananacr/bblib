e0a8f69e5c25803b9abd4139b4a17e8de07aa0fe
Ana Rodrigues <anananacr@gmail.com>
2023-09-08 10:49:39 +0200

    feat: cluster images by pixel histogram using k means

--- scripts/algorithms/k_means_clustering.py
+++ scripts/algorithms/k_means_clustering.py
@@ -36,17 +36,17 @@ def main():
 
     args = parser.parse_args()
 
-    data = np.load(f'{args.numpy_input}')
-    reduced_data=data[:10000,:]
-    norm_data=reduced_data.copy()
-    row,col=reduced_data.shape
+    data = np.load(f"{args.numpy_input}")
+    reduced_data = data[:10000, :]
+    norm_data = reduced_data.copy()
+    row, col = reduced_data.shape
     for i in range(col):
-        norm_factor=np.max(reduced_data[:,i])
-        if norm_factor>0:
-            norm_data[:,i]=norm_data[:,i]//norm_factor
+        norm_factor = np.max(reduced_data[:, i])
+        if norm_factor > 0:
+            norm_data[:, i] = norm_data[:, i] // norm_factor
 
     kmeans = KMeans(n_clusters=3).fit(reduced_data)
-    labels=kmeans.labels_
+    labels = kmeans.labels_
 
     # Obtain labels for each point in mesh. Use last trained model.
     Z = kmeans.predict(data)
@@ -55,16 +55,16 @@ def main():
     paths = files.readlines()
     files.close()
     for i in range(3):
-        f=open(f"{args.output}/lists/cluster_{i}.lst","w")
+        f = open(f"{args.output}/lists/cluster_{i}.lst", "w")
         f.close()
-    
-    for idx,i in enumerate(Z):
-        cmd=f"cp {paths[idx][:-1]} {args.output}/cluster_{i}"
+
+    for idx, i in enumerate(Z):
+        cmd = f"cp {paths[idx][:-1]} {args.output}/cluster_{i}"
         sub.call(cmd, shell=True)
-        f=open(f"{args.output}/lists/cluster_{i}.lst","a+")
+        f = open(f"{args.output}/lists/cluster_{i}.lst", "a+")
         f.write(f"{paths[idx]}")
         f.close()
-    
+
 
 if __name__ == "__main__":
     main()

35ddb4d2902a06c0bdc2adc606f62538eeb90568
Ana Rodrigues <anananacr@gmail.com>
2024-01-08 17:13:31 +0100

    fix: python env

--- scripts/algorithms/batch_center.sh
+++ scripts/algorithms/batch_center.sh
@@ -8,15 +8,14 @@
 #SBATCH --mem=40G
 
 #SBATCH --job-name  fwhm_center
-#SBATCH --output    /asap3/petra3/gpfs/p09/2023/data/11019087/scratch_cc/rodria/error/cc-%N-%j.out
-#SBATCH --error     /asap3/petra3/gpfs/p09/2023/data/11019087/scratch_cc/rodria/error/cc-%N-%j.err
+#SBATCH --output    /asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria/error/cc-%N-%j.out
+#SBATCH --error     /asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria/error/cc-%N-%j.err
 
 INPUT=$1
-ROOT=/asap3/petra3/gpfs/p09/2023/data/11019087/processed/rodria
-SCRATCH=/asap3/petra3/gpfs/p09/2023/data/11019087/scratch_cc/rodria
+ROOT=/asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria
+SCRATCH=/asap3/petra3/gpfs/p09/2023/data/11019088/scratch_cc/rodria
 
 source /etc/profile.d/modules.sh
-module load maxwell python/3.6
 source /home/rodria/scripts/p09/env-p09/bin/activate
 
-python find_center.py -i ${SCRATCH}/lists/${INPUT}.lst -o ${ROOT} -s ${SCRATCH} -m /asap3/petra3/gpfs/p09/2023/data/11019087/scratch_cc/rodria/masks/mask_ana_v4.h5 -m_bad /asap3/petra3/gpfs/p09/2023/data/11019087/scratch_cc/rodria/masks/mask_bad_pixels_v0.h5 -g /asap3/petra3/gpfs/p09/2023/data/11019087/processed/galchenm/beam_sweeping/powder_direct_beam/eiger500k_corrected_beam_centre.geom;
+python find_center.py -i ${ROOT}/lists/${INPUT} -o ${ROOT} -s ${SCRATCH} -m /asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria/mask/mask_ana_dec_v2.h5 -g /asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria/geoms/eiger500k_corrected_beam_centre.geom

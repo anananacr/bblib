3cd66bfc3099d00379cc91441f35171fffd67b80
Ana Rodrigues <anananacr@gmail.com>
2024-01-18 11:42:00 +0100

    feat: batch index using peakfinder8

---
+++ scripts/algorithms/batch_index_fosakp.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+#SBATCH --partition=allcpu,cfel
+#SBATCH --time=1-23:00:00
+#SBATCH --requeue
+#SBATCH --nodes=1
+#SBATCH --mincpus=128
+#SBATCH --mem=10G
+
+#SBATCH --job-name  index
+#SBATCH --output    /asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria/error/indexamajig-%N-%j.out
+#SBATCH --error     /asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria/error/indexamajig-%N-%j.err
+
+INPUT=$1
+OUTPUT=$2
+CENTER=$3
+INDEX=$4
+DIST=$5
+CELL=$6
+
+
+ROOT=/asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria
+
+source /etc/profile.d/modules.sh
+module purge
+module load maxwell crystfel/0.10.2
+
+rm /asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria/streams/${OUTPUT}.stream
+if [ "$CENTER" -eq 0 ];
+then
+    command="indexamajig -i ${ROOT}/lists/${INPUT}.lst -o  ${ROOT}/streams/${OUTPUT}.stream -g ${ROOT}/geoms/eiger500k_corrected_beam_centre_fosakp.geom --peaks=peakfinder8"
+else
+    if [ "$DIST" -gt 0 ];
+    then
+    command="indexamajig -i ${ROOT}/lists/${INPUT}.lst -o  ${ROOT}/streams/${OUTPUT}_${DIST}.stream -g ${ROOT}/geoms/eiger500k_corrected_beam_centre_shift_fosakp_${DIST}.geom --peaks=peakfinder8"
+    else
+    command="indexamajig -i ${ROOT}/lists/${INPUT}.lst -o  ${ROOT}/streams/${OUTPUT}.stream -g ${ROOT}/geoms/eiger500k_corrected_beam_centre_shift_fosakp.geom --peaks=peakfinder8"
+    fi
+fi
+
+if [ "$INDEX" -eq 0 ];
+then
+    command="$command --indexing=none"
+else
+    if [ -n "$CELL" ];
+    then
+    command="$command -p ${ROOT}/cell/${CELL}"
+    fi
+fi
+
+command="$command -j 128 --threshold=40 --min-snr=5 --min-pix-count=1 --max-pix-count=200 --max-res=1200 --int-radius=3,4,5 --min-peaks=10;"
+
+echo $command
+$command

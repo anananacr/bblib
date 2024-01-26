7d2f01a6e317c50c458ada98b221ddac21544465
Ana Rodrigues <anananacr@gmail.com>
2024-01-26 14:04:11 +0100

    fix: changes in indexing options

--- scripts/algorithms/batch_index_lyso.sh
+++ scripts/algorithms/batch_index_lyso.sh
@@ -23,7 +23,7 @@ module purge
 module load maxwell crystfel/0.10.2
 
 rm /asap3/petra3/gpfs/p09/2023/data/11019088/processed/rodria/streams/${OUTPUT}.stream
-command="indexamajig -i ${ROOT}/lists/${INPUT}.lst -o  ${ROOT}/streams/${OUTPUT}.stream -g ${ROOT}/geoms/eiger500k_corrected_beam_centre_lyso_for_index.geom  --peaks=cxi "
+command="indexamajig -i ${ROOT}/lists/${INPUT}.lst --indexing=xgandalf,asdf -o  ${ROOT}/streams/${OUTPUT}.stream -g ${ROOT}/geoms/eiger500k_corrected_beam_centre_shift_lyso_for_index.geom  --peaks=cxi --copy-header=/entry/data/shift_vertical_mm --copy-header=/entry/data/event_id --copy-header=/entry/data/file_id --copy-header=/entry/data/nPeaks --no-check-peaks --integration=rings-grad-nocen --int-radius=3,4,6 --no-refine --no-retry --no-revalidate"
 if [ -n "$CELL" ];
 then
 command="$command -p ${ROOT}/cell/${CELL}"

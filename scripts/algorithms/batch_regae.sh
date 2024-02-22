45c77dc92289577360dc303e09e8aa3271b2fa45
Ana Rodrigues <anananacr@gmail.com>
2024-02-22 15:19:27 +0100

    chore: change paths

--- scripts/algorithms/batch_regae.sh
+++ scripts/algorithms/batch_regae.sh
@@ -8,14 +8,14 @@
 #SBATCH --mem=10G
 #SBATCH --nice=100
 #SBATCH --job-name  bb
-#SBATCH --output    /asap3/fs-bmx/gpfs/regae/2023/data/11018148/processed/rodria/error/bb-%N-%j.out
-#SBATCH --error     /asap3/fs-bmx/gpfs/regae/2023/data/11018148/processed/rodria/error/bb-%N-%j.err
+#SBATCH --output   /asap3/fs-bmx/gpfs/regae/2022/data/11016614/processed/test/rodria/error/bb-%N-%j.out
+#SBATCH --error    /asap3/fs-bmx/gpfs/regae/2022/data/11016614/processed/test/rodria/error/bb-%N-%j.err
 
 INPUT=$1
-ROOT=/asap3/fs-bmx/gpfs/regae/2023/data/11018148/processed/rodria
-SCRATCH=/asap3/fs-bmx/gpfs/regae/2023/data/11018148/scratch_cc/rodria/test
+ROOT=/asap3/fs-bmx/gpfs/regae/2022/data/11016614/processed/test/rodria
+SCRATCH=/asap3/fs-bmx/gpfs/regae/2022/data/11016614/scratch_cc/rodria/test
 
 source /etc/profile.d/modules.sh
 source /home/rodria/scripts/p09/env-p09/bin/activate
 
-python find_center.py -i ${ROOT}/lists/${INPUT} -o ${ROOT}/../test -s ${SCRATCH} -g ${ROOT}/geoms/JF_regae_v4_altered.geom
+python find_center.py -i ${ROOT}/lists/${INPUT} -o ${ROOT}/.. -s ${SCRATCH} -g ${ROOT}/geoms/JF_regae_v4_altered.geom

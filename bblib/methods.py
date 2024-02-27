fix: detector center calculation for single panel

bblib/methods.py
@@ -66,7 +66,7 @@ class CenterOfMass(CenteringMethod):
 
         with h5py.File(f"{self.PF8Config.bad_pixel_map_filename}", "r") as f:
             mask = np.array(f[f"{self.PF8Config.bad_pixel_map_hdf5_path}"])
-            
+
         if (self.PF8Config.pf8_detector_info["nasics_x"] * self.PF8Config.pf8_detector_info["nasics_y"]
                 > 1
             ):
@@ -106,7 +106,7 @@ class CenterOfMass(CenteringMethod):
                 f'{self.plots_info["root_path"]}/center_refinement/plots/{self.plots_info["run_label"]}/center_of_mass/{self.plots_info["file_label"]}_{self.plots_info["frame_index"]}.png'
             )
             plt.close()
-        return np.round(center, 1)
+        return np.round(center, 0)
 
 
 class CircleDetection(CenteringMethod):

bblib/models.py
@@ -141,8 +141,8 @@ class PF8Info:
                 _img_center_y = int(visual_img_shape[0] / 2)
             else:
                 # Single panel
-                _img_center_x = self._data_shape[1] +1 - self.pixel_maps["x"][0, 0]
-                _img_center_y = self._data_shape[0] +1 - self.pixel_maps["y"][0, 0]
+                _img_center_x = int(abs(np.min(self.pixel_maps["x"])))
+                _img_center_y = int(abs(np.min(self.pixel_maps["y"])))
         else:
             _img_center_x = self._detector_center_from_geom[0] + self._detector_shift_x
             _img_center_y = self._detector_center_from_geom[1] + self._detector_shift_y
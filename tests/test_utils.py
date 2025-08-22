ca805ed5737a3be846b8bee3ab570a651bb46d21
anananacr <anananacr@gmail.com>
2025-08-22 16:19:04 -0300

    fix: bug fix in axis sum for projection

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,12 +1,31 @@
 from bblib.utils import get_fwhm_map_min_from_projection
-import numpy as np
-import os
 from csv import DictReader
 from bblib.utils import get_fwhm_map_min_from_projection
 
-def test_fwhm_map_min_from_projection():
+def test_type_output_from_fwhm_map_min_from_projection_without_plot():
+    with open("example/grid_search.csv", "r") as csv_file:
+        lines = list(DictReader(csv_file))
+
+    center = get_fwhm_map_min_from_projection(lines = lines, label = "", output_folder = "", pixel_step = 1, plots_flag = False)
+    assert  isinstance(center, list)
+
+def test_type_output_from_fwhm_map_min_from_projection_with_plot():
     with open("example/grid_search.csv", "r") as csv_file:
         lines = list(DictReader(csv_file))
 
     center = get_fwhm_map_min_from_projection(lines = lines, label = "test", output_folder = ".", pixel_step = 1, plots_flag = True)
     assert  isinstance(center, list)
+
+def test_output_value_from_fwhm_map_min_from_projection_without_plot():
+    with open("example/grid_search.csv", "r") as csv_file:
+        lines = list(DictReader(csv_file))
+
+    center = get_fwhm_map_min_from_projection(lines = lines, label = "", output_folder = "", pixel_step = 1, plots_flag = False)
+    assert  center == [7,13]
+
+def test_output_value_from_fwhm_map_min_from_projection_with_plot():
+    with open("example/grid_search.csv", "r") as csv_file:
+        lines = list(DictReader(csv_file))
+
+    center = get_fwhm_map_min_from_projection(lines = lines, label = "test", output_folder = ".", pixel_step = 1, plots_flag = True)
+    assert  center == [7,13]

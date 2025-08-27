500788ab9b213e62f962ac89e5d019f1fa9d26b8
anananacr <anananacr@gmail.com>
2025-08-27 15:18:18 -0300

    chore: test center of mass and friedel

---
+++ tests/test_methods_center_of_mass.py
@@ -0,0 +1,72 @@
+from bblib.methods import CenterOfMass
+from bblib.models import PF8Info
+import h5py
+import numpy as np
+
+config = {
+    "plots_flag": False,
+	"offset": {
+		"x": 0,
+		"y": 0
+		},
+	"peak_region":{
+		"min": 60,
+		"max": 180
+		},
+	"bragg_peaks_for_center_of_mass_calculation": 0,
+	"pixels_for_mask_of_bragg_peaks": 2
+    }
+
+plots_info = {
+    "filename": "test",
+	"folder_name": "test_center_of_mass",
+	"root_path": "/Users/minoru/Documents/ana/scripts/bblib/tests",
+	"value_auto": False,
+	"value_max": 100,
+	"value_min": 1,
+	"axis_lim_auto": True,
+	"xlim_min": 0,
+	"xlim_max": 0,
+	"ylim_min": 0,
+	"ylim_max": 0,
+	"color_map": "viridis",
+	"marker_size": 20
+}
+
+PF8Config = PF8Info()
+
+def test_type_output_from_center_of_mass_without_plot():
+    PF8Config.set_geometry_from_file("example/simple.geom")
+    center_of_mass_method = CenterOfMass(config=config, PF8Config=PF8Config, plots_info=plots_info)
+    with h5py.File("example/thin_glowing_ring_center_600_200.h5", "r") as f:
+        data = np.array(f["ring"][0])
+    center_from_center_of_mass = center_of_mass_method(data = data)
+    assert isinstance(center_from_center_of_mass, list)
+
+def test_type_output_from_center_of_mass_with_plot():
+    PF8Config.set_geometry_from_file("example/simple.geom")
+    config["plots_flag"]=True
+    center_of_mass_method = CenterOfMass(config=config, PF8Config=PF8Config, plots_info=plots_info)
+    with h5py.File("example/thin_glowing_ring_center_600_200.h5", "r") as f:
+        data = np.array(f["ring"][0])
+    center_from_center_of_mass = center_of_mass_method(data = data)
+    assert isinstance(center_from_center_of_mass, list)
+
+
+def test_output_from_center_of_mass_without_plot():
+    PF8Config.set_geometry_from_file("example/simple.geom")
+    center_of_mass_method = CenterOfMass(config=config, PF8Config=PF8Config, plots_info=plots_info)
+    with h5py.File("example/thin_glowing_ring_center_600_200.h5", "r") as f:
+        data = np.array(f["ring"][0])
+    center_from_center_of_mass = center_of_mass_method(data = data)
+    assert center_from_center_of_mass == [600,200]
+
+
+def test_output_from_center_of_mass_with_plot():
+    PF8Config.set_geometry_from_file("example/simple.geom")
+    config["plots_flag"]=True
+    center_of_mass_method = CenterOfMass(config=config, PF8Config=PF8Config, plots_info=plots_info)
+    with h5py.File("example/thin_glowing_ring_center_600_200.h5", "r") as f:
+        data = np.array(f["ring"][0])
+    center_from_center_of_mass = center_of_mass_method(data = data)
+    assert center_from_center_of_mass == [600,200]
---
+++ tests/test_methods_friedel_pairs.py
@@ -0,0 +1,66 @@
+from bblib.methods import FriedelPairs
+from bblib.models import PF8Info
+import h5py
+import numpy as np
+
+config = {
+    "plots_flag": False,
+	"search_radius": 10,
+	"polarization": {
+		"apply_polarization_correction": False,
+		"axis": "x",
+		"value": 0.99
+		}
+    }
+
+plots_info = {
+    "filename": "test",
+	"folder_name": "test_friedel_pairs",
+	"root_path": "/Users/minoru/Documents/ana/scripts/bblib/tests",
+	"value_auto": False,
+	"value_max": 100,
+	"value_min": 1,
+	"axis_lim_auto": True,
+	"xlim_min": 0,
+	"xlim_max": 0,
+	"ylim_min": 0,
+	"ylim_max": 0,
+	"color_map": "viridis",
+	"marker_size": 50
+}
+
+PF8Config = PF8Info()
+
+def test_type_output_from_friedel_pairs_without_plot():
+    PF8Config.set_geometry_from_file("example/simple.geom")
+    friedel_pairs_method = FriedelPairs(config=config, PF8Config=PF8Config, plots_info=plots_info)
+    with h5py.File("example/thin_glowing_ring_bragg_center_600_200.h5", "r") as f:
+        data = np.array(f["image"][0])
+    center_from_friedel_pairs = friedel_pairs_method(data = data, initial_guess = [604,200])
+    assert isinstance(center_from_friedel_pairs, list)
+
+def test_type_output_from_friedel_pairs_with_plot():
+    PF8Config.set_geometry_from_file("example/simple.geom")
+    config["plots_flag"]=True
+    friedel_pairs_method = FriedelPairs(config=config, PF8Config=PF8Config, plots_info=plots_info)
+    with h5py.File("example/thin_glowing_ring_bragg_center_600_200.h5", "r") as f:
+        data = np.array(f["image"][0])
+    center_from_friedel_pairs = friedel_pairs_method(data = data, initial_guess = [604,200])
+    assert isinstance(center_from_friedel_pairs, list)
+
+def test_output_from_friedel_pairs_without_plot():
+    PF8Config.set_geometry_from_file("example/simple.geom")
+    friedel_pairs_method = FriedelPairs(config=config, PF8Config=PF8Config, plots_info=plots_info)
+    with h5py.File("example/thin_glowing_ring_bragg_center_600_200.h5", "r") as f:
+        data = np.array(f["image"][0])
+    center_from_friedel_pairs = friedel_pairs_method(data = data, initial_guess = [604,200])
+    assert center_from_friedel_pairs == [600.5,200]
+
+def test_output_from_friedel_pairs_with_plot():
+    PF8Config.set_geometry_from_file("example/simple.geom")
+    config["plots_flag"]=True
+    friedel_pairs_method = FriedelPairs(config=config, PF8Config=PF8Config, plots_info=plots_info)
+    with h5py.File("example/thin_glowing_ring_bragg_center_600_200.h5", "r") as f:
+        data = np.array(f["image"][0])
+    center_from_friedel_pairs = friedel_pairs_method(data = data, initial_guess = [604,200])
+    assert center_from_friedel_pairs == [600.5,200]

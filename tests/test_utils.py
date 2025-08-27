fea7cdd6a7a4919e3b9566794808ceffc2aa0a21
anananacr <anananacr@gmail.com>
2025-08-27 16:31:05 -0300

    chore: black

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,30 +1,50 @@
 from bblib.utils import get_fwhm_map_min_from_projection
 from csv import DictReader
 
+
 def test_type_output_from_fwhm_map_min_from_projection_without_plot():
     with open("example/grid_search.csv", "r") as csv_file:
         lines = list(DictReader(csv_file))
 
-    center = get_fwhm_map_min_from_projection(lines = lines, label = "", output_folder = "", pixel_step = 1, plots_flag = False)
-    assert  isinstance(center, list)
+    center = get_fwhm_map_min_from_projection(
+        lines=lines, label="", output_folder="", pixel_step=1, plots_flag=False
+    )
+    assert isinstance(center, list)
+
 
 def test_type_output_from_fwhm_map_min_from_projection_with_plot():
     with open("example/grid_search.csv", "r") as csv_file:
         lines = list(DictReader(csv_file))
 
-    center = get_fwhm_map_min_from_projection(lines = lines, label = "test", output_folder = "center_refinement/plots/test_utils", pixel_step = 1, plots_flag = True)
-    assert  isinstance(center, list)
+    center = get_fwhm_map_min_from_projection(
+        lines=lines,
+        label="test",
+        output_folder="center_refinement/plots/test_utils",
+        pixel_step=1,
+        plots_flag=True,
+    )
+    assert isinstance(center, list)
+
 
 def test_output_value_from_fwhm_map_min_from_projection_without_plot():
     with open("example/grid_search.csv", "r") as csv_file:
         lines = list(DictReader(csv_file))
 
-    center = get_fwhm_map_min_from_projection(lines = lines, label = "", output_folder = "", pixel_step = 1, plots_flag = False)
-    assert  center == [400,600]
+    center = get_fwhm_map_min_from_projection(
+        lines=lines, label="", output_folder="", pixel_step=1, plots_flag=False
+    )
+    assert center == [400, 600]
+
 
 def test_output_value_from_fwhm_map_min_from_projection_with_plot():
     with open("example/grid_search.csv", "r") as csv_file:
         lines = list(DictReader(csv_file))
 
-    center = get_fwhm_map_min_from_projection(lines = lines, label = "test", output_folder = "center_refinement/plots/test_utils", pixel_step = 1, plots_flag = True)
-    assert  center == [400,600]
+    center = get_fwhm_map_min_from_projection(
+        lines=lines,
+        label="test",
+        output_folder="center_refinement/plots/test_utils",
+        pixel_step=1,
+        plots_flag=True,
+    )
+    assert center == [400, 600]
